5a85b4426fad1f3a6bf07ba59d21c49cd9ce7798
raphaeldodier <raphael.dodier@outlook.com>
2020-03-20 18:15:51 -0400

    typographie

--- functions.php
+++ functions.php
@@ -4,3 +4,10 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
  
 }
+
+function wpt_add_google_fonts() {
+ 
+    wp_enqueue_style( 'wpt-google-fonts', 'https://fonts.googleapis.com/css?family=Baloo+Da+2&display=swap', false ); 
+    }
+     
+    add_action( 'wp_enqueue_scripts', 'wpt_add_google_fonts' );
